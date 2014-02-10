ae0e5dad6fc5cc1f74b971d39a9915a797e5dba8
Jimi van der Woning <jimivdwoning@gmail.com>
2014-02-10 17:40:23 +0100

    Fixed O IACM conversion error; aromatic Os are now of type 1.

--- src/omfraf/main/util.py
+++ src/omfraf/main/util.py
@@ -88,7 +88,7 @@ class Atom:
       elif len(bas) > 1:
         return 3
       elif bas and len(filter(lambda a: a.element == 'O', \
-          bas[0].get_bonded_atoms())) > 1:
+          bas[0].get_bonded_atoms())) > 1 and bas != self.get_bonded_atoms(5):
         return 2
       else:
         return 1
