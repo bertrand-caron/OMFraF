f3aefd9e0a6cba05e9d33cd9fd2d58df22fd6010
Bertrand Caron <bertrand.fx.caron@gmail.com>
2016-09-29 08:48:25 +1000

    Feature: Migrate to django-1.10, hardcode expected version

--- src/manage.py
+++ src/manage.py
@@ -2,7 +2,17 @@
 import os
 import sys
 
+SUPPORTED_DJANGO_VERSION = (1, 10)
+
 if __name__ == "__main__":
+    from django import VERSION
+    if not all([(expected == current) for (expected, current) in zip(SUPPORTED_DJANGO_VERSION, VERSION)]):
+        raise Exception(
+            'ERROR: Wrong Django version. Use django-{0}'.format(
+                '.'.join(map(str, SUPPORTED_DJANGO_VERSION)),
+            ),
+        )
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omfraf.settings")
 
     from django.core.management import execute_from_command_line
