3dbbd95ec159040a3bb820d5480a943bc1d4850e
Bertrand Caron <bertrand.fx.caron@gmail.com>
2016-09-29 14:01:18 +1000

    Feature: Allow serving on relative url

--- src/omfraf/urls.py
+++ src/omfraf/urls.py
@@ -1,9 +1,15 @@
+from os.path import join
 from django.conf.urls import include, url
 import omfraf.main.views as views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+
+RELATIVE_URL = r'^omfraf'
+
+def relative_url(url):
+    return join(RELATIVE_URL, url) if bool(url) else RELATIVE_URL
 
 urlpatterns = [
     # Examples:
@@ -15,9 +21,9 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
-    url(r'^repos/$', views.repos, name='repos'),
-    url(r'^generate/$', views.generate, name='generate'),
-    url(r'^load/$', views.load, name='load'),
-    url(r'^update_mop/$', views.update_mop, name='update_mop'),
+    url(relative_url(r'$'), views.index, name='index'),
+    url(relative_url(r'repos/$'), views.repos, name='repos'),
+    url(relative_url(r'generate/$'), views.generate, name='generate'),
+    url(relative_url(r'load/$'), views.load, name='load'),
+    url(relative_url(r'update_mop/$'), views.update_mop, name='update_mop'),
 ]
